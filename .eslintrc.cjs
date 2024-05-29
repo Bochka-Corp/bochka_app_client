33274cc3da129ce40cfab86252103fb5bf300860
Bogdan Isachenko <egor1414148@gmail.com>
2024-05-29 03:12:53 +0300

    feat: added autofill in search

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,10 @@ module.exports = {
     'import/extensions': 'off',
     'react/prop-types': 'off',
     'no-return-assign': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this' : 'off',
+    'prefer-promise-reject-errors': 'off',
+    'react/no-array-index-key': 'off',
     'jsx-a11y/control-has-associated-label': 'off'
   },
 }
---
+++ src/utils/Api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+class Api {
+  constructor({ baseUrl }) {
+    this._baseUrl = baseUrl;
+  }
+
+  _checkResponse(response) {
+    if (response.status === 200) {
+      return response.data;
+    }
+
+    return Promise.reject(`Ошибка: ${response.status}`);
+  }
+
+  getSuggestions(search) {
+    return axios.get(`https://justcors.com/tl_46d8d6d/https://suggest-maps.yandex.ru/v1/suggest?apikey=0e42c64c-0045-4b29-ba5d-cb9f4001bc0a&types=locality&results=5&text=${search}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => this._checkResponse(res));
+  }
+}
+
+const api = new Api({
+  baseUrl: '',
+});
+
+export default api;
